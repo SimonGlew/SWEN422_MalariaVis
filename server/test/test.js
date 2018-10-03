d7b89275e0d9508b0e9163ac30781824afaf7a73
glewsimo <glewsimo@ecs.vuw.ac.nz>
2018-10-03 13:19:55 +1300

    added based test stuff

---
+++ server/test/test.js
@@ -0,0 +1,32 @@
+var webdriver = require('selenium-webdriver');
+
+// Input capabilities
+var capabilities = {
+ 'browserName' : 'Chrome',
+ 'browser_version' : '62.0',
+ 'os' : 'Windows',
+ 'os_version' : '10',
+ 'resolution' : '1024x768',
+ 'browserstack.user' : 'markharris16',
+ 'browserstack.key' : '1Qxh5CEZexSskYyGuGTc'
+}
+
+var driver = new webdriver.Builder().
+  usingServer('http://hub-cloud.browserstack.com/wd/hub').
+  withCapabilities(capabilities).
+  build();
+
+driver.get('http://www.google.com').then(function(){
+    console.log('fmiowrengpreo')
+//   driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function(){
+//     driver.getTitle().then(function(title) {
+//       console.log(title);
+//       driver.quit();
+//     });
+//   });
+});
+
+//BROWSER STACK LOGIN DETAILS
+
+//email: t5.swen422@gmail.com
+//password: BQBhWeaK
