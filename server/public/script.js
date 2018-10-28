4ccf39e938cb72c2ad3c5eb367b379c5c710b753
carrjona <carrjona@ecs.vuw.ac.nz>
2018-10-29 12:53:40 +1300

    Implemented basic map

--- server/public/script.js
+++ server/public/script.js
@@ -8,5 +8,25 @@ d3.json(server + "/worldmap.json").then(function(data){
 })
 
 function drawMap(){
+  var mapsvg = d3.select("#map");
+  var mapWidth = parseInt(mapsvg.style("width"));
+  var mapHeight = parseInt(mapsvg.style("height"));
+  var margin = 5;
+  var masterProject = d3.geoMercator();
+  masterProject.fitExtent([[margin, margin], [mapWidth, mapHeight]], mapData);
+  path = d3.geoPath().projection(masterProject);
+  
+  mapsvg.append("g")
+   .attr("class", "worldmap")
+   .selectAll("path")
+   .data(mapData.features)
+   .enter()
+   .append("path")
+     .attr("d", path)
+     .attr("fill", "#333")
+     .attr("stroke-width", 0.5)
+     .attr("stroke", "#000")
+     .attr("pointer-events", "none")
+
 
 }
