27d4d94834a7a262e711182ba2a44ba6ddfd34f1
carrjona <carrjona@ecs.vuw.ac.nz>
2018-11-09 12:09:45 +1300

    Added titles and axis labels to line chart

--- server/public/js/linegraph.js
+++ server/public/js/linegraph.js
@@ -4,7 +4,7 @@ var lineWidth, margin = 50;
 var legendHeight = 20, legendWidth = 50, legendPadding = 5;
 var xScale, yScale;
 
-var legendColors = ['#7fc97f','#beaed4','#fdc086','#ffff99','#386cb0','#f0027f'];
+var legendColors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'];
 var data = [];
 
 var line = d3.line()
@@ -22,6 +22,13 @@ function drawChart(){
   svgWidth = chartBbox.width;
   svgHeight = chartBbox.height;
 
+  linesvg.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("fill", "#FFF")
+
   lineWidth = 0.7*svgWidth;
 
   xScale = d3.scaleLinear()
@@ -29,7 +36,8 @@ function drawChart(){
     .range([0, lineWidth - margin*2])
 
   var xAxis = d3.axisBottom()
-    .scale(xScale);
+    .scale(xScale)
+    .ticks(4, "d");
 
   yScale = d3.scaleLinear()
     .domain([1000, 0])
@@ -43,11 +51,46 @@ function drawChart(){
     .attr("transform", "translate( " + 2*margin + ", " + margin + ")")
     .call(yAxis);
 
-  //TODO: Format ticks
   linesvg.append("g")
     .attr("class", "xAxis")
     .attr("transform", "translate( " + 2*margin + ", " + (svgHeight - margin) + ")")
     .call(xAxis);
+
+  linesvg.append("text")
+    .attr("id", "line-title")
+    .attr("x",  margin*2 + (lineWidth - 2*margin)/2)
+    .attr("y", margin/2)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central")
+    .text("Malaria Incidence Rates, 2000 - 2015")
+
+  linesvg.append("text")
+    .attr("id", "line-xLabel")
+    .attr("x", margin*2 + (lineWidth - 2*margin)/2)
+    .attr("y", svgHeight - margin/2)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central")
+    .text("Year")
+
+  linesvg.append("text")
+    .attr("id", "line-yLabel")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central")
+    .attr("transform", "rotate(-90)translate(" + (-svgHeight/2) + "," + (margin/2) + ")")
+    .text("Incidence Rate")
+
+  linesvg.append("text")
+    .attr("id", "line-yLabel")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("text-anchor", "middle")
+    .attr("font-size", 13)
+    .attr("alignment-baseline", "central")
+    .attr("transform", "rotate(-90)translate(" + (-svgHeight/2) + "," + (margin) + ")")
+    .text("(per 100,000 population)")
+
 }
 
 function addToChart(feature){
@@ -74,22 +117,21 @@ function addToChart(feature){
     .attr("class", "legendrect")
     .attr("id", "legendrect-" + feature.properties.adm0_a3)
     .attr("x", lineWidth + margin)
-    .attr("y", margin + (data.length-1) * (legendPadding + legendHeight))
+    .attr("y", margin + (data.length) * (legendPadding + legendHeight))
     .attr("width", legendWidth)
     .attr("height", legendHeight)
-    .attr("fill", color);
+    .attr("fill", color)
+    .style("opacity", 0);
 
   linesvg.append("text")
     .datum({id: feature.properties.adm0_a3})
     .attr("class", "legendtext")
     .attr("id", "legendtext-" + feature.properties.adm0_a3)
     .attr("x", lineWidth + margin + legendWidth + legendPadding)
-    .attr("y", margin + (data.length-1) * (legendPadding + legendHeight) + legendHeight/2)
+    .attr("y", margin + (data.length) * (legendPadding + legendHeight) + legendHeight/2)
     .text(feature.properties.brk_name)
-    .style("alignment-baseline", "central");
-
-
-    console.log(feature.properties.incidenceRates)
+    .style("alignment-baseline", "central")
+    .style("opacity", 0);
 
   //Add line
   linesvg.append("path")
@@ -105,6 +147,30 @@ function addToChart(feature){
     .attr("fill", "none")
     .attr("stroke-width", 2)
     .attr("stroke", color)
+
+  //Add circles
+  linesvg.append("g")
+    .attr("id", "markers-" + feature.properties.adm0_a3)
+    .selectAll("circle")
+    .data(feature.properties.incidenceRates)
+    .enter()
+    .append("circle")
+      .attr("cx", function(d){
+        return margin * 2 + xScale(d.year)
+      })
+      .attr("cy", margin + yScale(0))
+      .attr("r", 5)
+      .attr("fill", color)
+      .attr("stroke", "#FFF")
+      .attr("stroke-width", 2)
+      .style("opacity", 0)
+      .transition()
+      .attr("cy", function(d){
+        return margin + yScale(d.Value)
+      })
+      .style("opacity", 1)
+
+    redrawLegend();
 }
 
 function removeFromChart(feature){
@@ -126,7 +192,13 @@ function removeFromChart(feature){
     .on("end", function(){
       d3.select(this).remove();
     })
-
+  d3.select("#markers-" + feature.properties.adm0_a3).selectAll("circle")
+    .transition()
+    .attr("cy", margin + yScale(0))
+    .style("opacity", 0)
+    .on("end", function(){
+      d3.select("#markers-" + feature.properties.adm0_a3).remove();
+    })
 
   redrawLegend();
 }
@@ -141,6 +213,7 @@ function redrawLegend(){
         }
       }
     })
+    .style("opacity", 1)
 
   d3.selectAll(".legendrect")
     .transition()
@@ -151,6 +224,7 @@ function redrawLegend(){
         }
       }
     })
+    .style("opacity", 1)
 }
 
 
